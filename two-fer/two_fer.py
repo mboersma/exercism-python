2c74a8a7d204406bf371827c6b788aa86f9b22df
Matt Boersma <matt@sprout.org>
2019-03-19 13:32:03 -0600

    [two-fer] all tests passing

--- two-fer/two_fer.py
+++ two-fer/two_fer.py
@@ -1,2 +1,6 @@
-def two_fer(name):
-    pass
+"""Two-fer or 2-fer is short for two for one. One for you and one for me."""
+
+
+def two_fer(name="you"):
+    """Return a personalized message of how the two items will be allocated."""
+    return "One for %s, one for me." % name
