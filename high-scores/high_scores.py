bb7e4c8d1b2e79a1dbd5173143631ddcaaaa9601
Matt Boersma <matt@sprout.org>
2019-03-20 13:51:05 -0600

    [high-scores] all tests passing

--- high-scores/high_scores.py
+++ high-scores/high_scores.py
@@ -1,3 +1,20 @@
+"""MAnage a game player's High Score list."""
+
+
 class HighScores(object):
+    """Reports the latest, best, and top three scores of those given."""
+
     def __init__(self, scores):
-        pass
+        self.scores = scores
+
+    def latest(self):
+        """Return the latest entry in the scores."""
+        return self.scores[-1]
+
+    def personal_best(self):
+        """Return the largest entry in the scores."""
+        return max(self.scores)
+
+    def personal_top_three(self):
+        """Return the top three entries in the scores, highest to lowest."""
+        return sorted(self.scores, reverse=True)[:3]
