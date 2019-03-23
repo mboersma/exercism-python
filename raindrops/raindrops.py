91f2f2d5c8b705982e9d99c76558c8e6c20677db
Matt Boersma <matt@sprout.org>
2019-03-23 10:17:03 -0600

    [raindrops] all tests passing

--- raindrops/raindrops.py
+++ raindrops/raindrops.py
@@ -1,2 +1,21 @@
+"""
+Convert a number to a string, the contents of which depend on the number's
+factors.
+"""
+
+
 def raindrops(number):
-    pass
+    """Return raindrop sounds when a number has factors of 3, 5, or 7."""
+    drops = ""
+
+    if number % 3 == 0:
+        drops += "Pling"
+    if number % 5 == 0:
+        drops += "Plang"
+    if number % 7 == 0:
+        drops += "Plong"
+
+    if not drops:
+        drops = str(number)
+
+    return drops
