ab89731ffac986febe7edfa23ea4d608f98257f8
Matt Boersma <matt@sprout.org>
2019-03-23 15:00:20 -0600

    [word-count] document as a verbose regex

--- word-count/word_count.py
+++ word-count/word_count.py
@@ -1,10 +1,15 @@
 """Given a phrase, count the occurrences of each word in that phrase."""
 
 from collections import Counter
-from re import findall
+import re
 
 
 def word_count(phrase):
     """Return a dict counting the occurrences of each word in a phrase."""
-    pattern = r"[a-zA-Z0-9]+(?<!\\)[a-zA-Z']*(?<!')"
-    return Counter(findall(pattern, phrase.lower()))
+    pattern = r"""
+    [a-zA-Z0-9]+  # one or more alphanumeric characters
+    (?<!\\)       # not preceded by a backslash
+    [a-zA-Z']*    # zero or more alphabetic characters or an apostrophe
+    (?<!')        # not preceded by an apostrophe
+    """
+    return Counter(re.findall(pattern, phrase.lower(), re.VERBOSE))
