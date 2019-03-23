927dc46b4b5521eda7780f41da11fcd886797f69
Matt Boersma <matt@sprout.org>
2019-03-23 17:36:58 -0600

    [grade-school] all tests passing

--- grade-school/grade_school.py
+++ grade-school/grade_school.py
@@ -1,12 +1,29 @@
+"""
+Given students' names along with the grade that they are in, create a roster
+for the school.
+"""
+
+from collections import defaultdict
+
+
 class School(object):
+    """Models the roster of students' names and grades."""
+
     def __init__(self):
-        pass
+        self._roster = defaultdict(list)
 
     def add_student(self, name, grade):
-        pass
+        """Add a student to the school roster."""
+        self._roster[grade].append(name)
+        self._roster[grade].sort()
 
     def roster(self):
-        pass
+        """Return the school student roster, sorted by grade and name."""
+        students = []
+        for grade_number in sorted(self._roster):
+            students.extend(self._roster[grade_number])
+        return students
 
     def grade(self, grade_number):
-        pass
+        """Return the students within a grade."""
+        return self._roster[grade_number]
