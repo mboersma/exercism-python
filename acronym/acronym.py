6b22df14557d22f3ef6981f4e9d8663dd10d5d61
Matt Boersma <matt@sprout.org>
2019-03-23 15:41:34 -0600

    [acronym] all tests passing

--- acronym/acronym.py
+++ acronym/acronym.py
@@ -1,2 +1,12 @@
+"""Convert a phrase to its acronym."""
+
+import re
+
+
 def abbreviate(words):
-    pass
+    """Return the abbreviation—or acronym—for the words provided."""
+    pattern = r"""
+    ([A-Z])  # capture the first capital letter
+    [A-Z']*  # followed by zero or more capital letters or apostrophes
+    """
+    return ''.join(re.findall(pattern, words.upper(), re.VERBOSE))
