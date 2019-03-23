4978db750d04e448e1c4f413e58a3b49c0be63d9
Matt Boersma <matt@sprout.org>
2019-03-23 10:47:02 -0600

    [isogram] all tests passing

--- isogram/isogram.py
+++ isogram/isogram.py
@@ -1,2 +1,9 @@
+"""Determine if a word or a phrase is an isogram."""
+
+
 def is_isogram(string):
-    pass
+    """Return True if a string contains no repeating letters, ignoring
+    spaces and hyphens."""
+    letters = string.replace('-', '').replace(' ', '').lower()
+    counts = {c: letters.count(c) for c in letters}
+    return all(v == 1 for v in counts.values())
