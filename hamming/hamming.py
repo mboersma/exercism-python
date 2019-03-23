89d54554ec7c5b5da0265f14481c7e5506bf35b2
Matt Boersma <matt@sprout.org>
2019-03-23 09:35:10 -0600

    [hamming]: all tests passing

--- hamming/hamming.py
+++ hamming/hamming.py
@@ -1,2 +1,10 @@
+"""Calculate the Hamming Distance between two DNA strands."""
+
+
 def distance(strand_a, strand_b):
-    pass
+    """Return the Hamming Distance calculated between two DNA strands
+    represented as strings."""
+    if len(strand_a) != len(strand_b):
+        raise ValueError("DNA strands must be of equal length.")
+
+    return sum(1 for x, y in zip(strand_a, strand_b) if x != y)
