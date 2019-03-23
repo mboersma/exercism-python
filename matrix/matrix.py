95718e62e0f3e3327375adef10cd2ea9ab4fef0d
Matt Boersma <matt@sprout.org>
2019-03-23 09:14:12 -0600

    [matrix] all tests passing

--- matrix/matrix.py
+++ matrix/matrix.py
@@ -1,9 +1,22 @@
+"""
+Given a string representing a matrix of numbers, return the rows
+and columns of that matrix.
+"""
+
+
 class Matrix(object):
+    """A 2-d matrix constructed from a string of numbers."""
+
     def __init__(self, matrix_string):
-        pass
+        self._matrix = []
+        for line in matrix_string.splitlines():
+            row = [int(n) for n in line.split()]
+            self._matrix.append(row)
 
     def row(self, index):
-        pass
+        """Return the matrix row at the given 1-based index."""
+        return self._matrix[index-1]
 
     def column(self, index):
-        pass
+        """Return the matrix column at the given 1-based index."""
+        return [row[index-1] for row in self._matrix]
