894dbf60f258961ded5d0ef4e66dec354f597476
Matt Boersma <matt@sprout.org>
2019-03-23 14:46:34 -0600

    [word-count] all tests passing

--- word-count/word_count.py
+++ word-count/word_count.py
@@ -1,2 +1,10 @@
+"""Given a phrase, count the occurrences of each word in that phrase."""
+
+from collections import Counter
+from re import findall
+
+
 def word_count(phrase):
-    pass
+    """Return a dict counting the occurrences of each word in a phrase."""
+    pattern = r"[a-zA-Z0-9]+(?<!\\)[a-zA-Z']*(?<!')"
+    return Counter(findall(pattern, phrase.lower()))
