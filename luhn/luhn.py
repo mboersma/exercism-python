c6268bac88d1401c893b09be8869ccfdd3b0a60d
Matt Boersma <matt@sprout.org>
2019-03-28 14:03:00 -0600

    [luhn] all tests passing

--- luhn/luhn.py
+++ luhn/luhn.py
@@ -1,6 +1,21 @@
+"""Given a number determine whether or not it is valid per the Luhn formula."""
+
+
 class Luhn(object):
+    """Luhn accepts a card number and validates it by a checksum formula."""
+
     def __init__(self, card_num):
-        pass
+        self.card_num = card_num
 
     def is_valid(self):
-        pass
+        """Return True if a number passes the Luhn checksum formula."""
+
+        def luhnify(digit):
+            return sum(divmod(digit*2, 10))
+
+        card_num = self.card_num.replace(' ', '')
+        if len(card_num) < 2 or not card_num.isdigit():
+            return False
+        digits = [int(c) for c in card_num]
+        odds, evens = digits[-2::-2], digits[-1::-2]
+        return sum([luhnify(d) for d in odds] + evens) % 10 == 0
