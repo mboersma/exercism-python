e1f9833f225b123ced38fd62991e798441df9963
Matt Boersma <matt@sprout.org>
2019-04-01 10:39:18 -0600

    [clock] all tests passing

--- clock/clock.py
+++ clock/clock.py
@@ -1,15 +1,31 @@
+"""Implements a clock that handles times without dates."""
+
+
+HOURS_PER_DAY = 24
+MINUTES_PER_HOUR = 60
+
+
 class Clock(object):
+    """A simple clock that supports addition, subtraction, and equality."""
+
     def __init__(self, hour, minute):
-        pass
+        self.hour = hour
+        self._rationalize(minute)
 
     def __repr__(self):
-        pass
+        return "{:02d}:{:02d}".format(self.hour, self.minute)
 
     def __eq__(self, other):
-        pass
+        return self.hour == other.hour and self.minute == other.minute
 
     def __add__(self, minutes):
-        pass
+        self._rationalize(self.minute + minutes)
+        return self
 
     def __sub__(self, minutes):
-        pass
+        self._rationalize(self.minute - minutes)
+        return self
+
+    def _rationalize(self, minutes):
+        quotient, self.minute = divmod(minutes, MINUTES_PER_HOUR)
+        self.hour = (self.hour + quotient) % HOURS_PER_DAY
